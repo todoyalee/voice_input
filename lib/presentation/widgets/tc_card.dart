6a238387227a708195fb2d909b9ff512774ca30d
Mohamed Ali Belkouri <medali.b@devnet.me>
2024-08-16 20:33:36 +0100

    ASR_in_flutter

--- lib/presentation/widgets/tc_card.dart
+++ lib/presentation/widgets/tc_card.dart
@@ -1,4 +1,5 @@
-import 'dart:ffi';
+import 'dart:io' if (dart.library.html) 'dart:html' as platform;
+
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
